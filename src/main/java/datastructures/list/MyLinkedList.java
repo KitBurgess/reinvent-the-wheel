f38142609bc842241a5f89c271a4319eddbbfdd1
christoper burgess <christopher.burgess@vortexa.com>
2018-04-23 23:00:34 +0100

    Add boilerplate linked list code

---
+++ src/main/java/datastructures/list/MyLinkedList.java
@@ -0,0 +1,38 @@
+package datastructures.list;
+
+public class MyLinkedList<E> implements MyList<E> {
+
+    public MyLinkedList() {
+
+    }
+
+    public MyLinkedList(int size) {
+
+    }
+
+    public void append(E entry) {
+
+    }
+
+    public void prepend(E entry) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public boolean isempty() {
+        return false;
+    }
+
+    @Override
+    public E getHead() {
+        return null;
+    }
+
+    @Override
+    public E getElement(int index) {
+        return null;
+    }
+}
---
+++ src/test/java/datastructures/list/MyLinkedListTest.java
@@ -0,0 +1,32 @@
+package datastructures.list;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MyLinkedListTest {
+    @Test
+    public void testAppend() {
+    }
+
+    @Test
+    public void testPrepend() {
+    }
+
+    @Test
+    public void testSize() {
+    }
+
+    @Test
+    public void testIsempty() {
+    }
+
+    @Test
+    public void testGetHead() {
+    }
+
+    @Test
+    public void testGetElement() {
+    }
+}
